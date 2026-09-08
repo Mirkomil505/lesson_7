a69af6334df0887a1c6c8755a059806377268d11
Mirkomil <makhmudovmirkomil22@gmail.com>
2026-09-08 20:33:55 +0500

    your message here

---
+++ src/main/java/uz/pdp/Book.java
@@ -0,0 +1,7 @@
+package uz.pdp;
+
+public class Book {
+    static void main() {
+
+    }
+}
--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -2,9 +2,9 @@ package uz.pdp;
 public class Main {
     static void main() {
 
-
-
-
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+        }
 
 
         }
