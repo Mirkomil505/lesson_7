b1d8c2d0b237eb43e2a16d4aa7a7861481ab5e93
Mirkomil <makhmudovmirkomil22@gmail.com>
2026-09-08 20:40:55 +0500

    hi

---
+++ src/main/java/uz/pdp/Homework.java
@@ -0,0 +1,12 @@
+package uz.pdp;
+
+public class Homework {
+    static void main() {
+
+
+        String age = "9";
+
+        System.out.println(age);
+
+    }
+}
--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -7,6 +7,10 @@ public class Main {
         }
 
 
+        String age = "9";
+
+        System.out.println(age);
+
         }
     }
 
